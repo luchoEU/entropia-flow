2e528492719de3092846dc8a983e435c47207b26
luchoEU <luchomuchoireton@gmail.com>
2022-02-11 08:46:14 -0300

    Add background to stream

--- chrome-extension/src/view/components/monitor/StreamView.tsx
+++ chrome-extension/src/view/components/monitor/StreamView.tsx
@@ -2,17 +2,23 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import { getLast } from '../../application/selectors/last'
 import { getStatus } from '../../application/selectors/status';
+import useScript from '../hooks/useScript';
+import CSS from 'csstype';
 
 function StreamView() {
     const { delta, deltaClass, deltaWord } = useSelector(getLast)
     const { message } = useSelector(getStatus);    
+    useScript('effect/ashfall/main.js')
+
+    const streamStyles: CSS.Properties = {
+        color: 'white',
+    }
 
     return (
         <section>
-            <h1>Stream View</h1>
-            <div className='stream-view'>
+            <div id="stream" className='stream-view' style={streamStyles}>
                 <div className='stream-frame'>
-                    <img className='stream-logo' src='img/flow128.png'></img>
+                    <img className='stream-logo' src='img/flow128w.png'></img>
                     <div className='stream-title'>Entropia Flow</div>
                     <div className='stream-subtitle'>Chrome Extension</div>
                     <div className={`stream-difference difference ${deltaClass}`}>{delta} PED {deltaWord}</div>
