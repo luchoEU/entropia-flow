f212bfa97a8412325ba0f674f82483b6e8353769
luchoEU <luchomuchoireton@gmail.com>
2022-07-02 16:40:32 -0300

    Add notification when sold

--- chrome-extension/src/view/application/middleware/history.ts
+++ chrome-extension/src/view/application/middleware/history.ts
@@ -1,7 +1,7 @@
-import { setHistoryExpanded, SET_HISTORY_EXPANDED } from "../actions/history"
+import { setHistoryExpanded, SET_HISTORY_EXPANDED, SET_HISTORY_LIST } from "../actions/history"
 import { PAGE_LOADED } from "../actions/ui"
 import { getHistory } from "../selectors/history"
-import { HistoryState } from "../state/history"
+import { HistoryState, MindEssenceLogText, ViewItemData } from "../state/history"
 
 const requests = ({ api }) => ({ dispatch, getState }) => next => async (action) => {
     next(action)
@@ -10,6 +10,20 @@ const requests = ({ api }) => ({ dispatch, getState }) => next => async (action)
             const expanded = await api.storage.loadHistoryExpanded()
             if (expanded)
                 dispatch(setHistoryExpanded(expanded))
+            break
+        }
+        case SET_HISTORY_LIST: {
+            const state: HistoryState = getHistory(getState())
+            if (state.list[0].diff) {
+                var reduced = state.list[0].diff.flatMap((i: ViewItemData) =>
+                    i.a !== undefined ? [ MindEssenceLogText[i.a.type] ] : []);
+                if (reduced.length > 0) {
+                    chrome.notifications.create(
+                        undefined,
+                        { type: "basic", iconUrl: "img/flow128.png", title: "Entropia Flow", message: reduced.join('\n') }
+                    )
+                }
+            }
             break
         }
         case SET_HISTORY_EXPANDED: {
