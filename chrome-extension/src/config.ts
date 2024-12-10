131bf68d9b1edd2d728b0fad356ec69ac4a6b5ee
luchoEU <luchomuchoireton@gmail.com>
2024-12-10 19:37:48 +0100

    Add config for tt service

--- chrome-extension/src/config.ts
+++ chrome-extension/src/config.ts
@@ -1,3 +1,6 @@
 export const SHOW_FEATURES_IN_DEVELOPMENT = false
+
+export const SHOW_TT_SERVICE = SHOW_FEATURES_IN_DEVELOPMENT
+
 export const SHOW_BUDGET_IN_CRAFT = false
 export const CLEAR_WEB_ON_LOAD = false
